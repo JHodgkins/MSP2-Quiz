a673b1d5f1b4df88a14385308d8224c7907fbdd5
JHodgkins <jhodgkins78@gmail.com>
2022-05-14 23:55:55 +0000

    code: play now event listener added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('script file loaded');
+// function URL() {
+//     location.href = 'https://youtube.com';
+// }
+document.getElementById("play_now").onclick = function () {
+    location.href = "game.html";
+};
