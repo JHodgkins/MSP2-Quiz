168decf2d6c366bbcef831372b52784f6f300043
JHodgkins <jhodgkins78@gmail.com>
2022-05-15 18:04:22 +0000

    code: get questions array length

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,10 @@ const questionChoiceB = document.getElementById("B");
 const questionChoiceC = document.getElementById("C");
 const questionChoiceD = document.getElementById("D");
 
+// get total amount of questions and set a variable to hold current question index
+const totalQuestionsLength = questions.length -1;
+let currentQuestion = 0;
+
 // Game start
 function gameStart() {
     intro.style.display = 'none';
