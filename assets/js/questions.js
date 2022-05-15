3475bd937438f775dfde34ae7cc739e5b7c05824
JHodgkins <jhodgkins78@gmail.com>
2022-05-15 17:53:01 +0000

    code: questions array file added

---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+        question: "what two colours make green??",
+        choiceA: "red and blue",
+        choiceB: "blue and purple",
+        choiceC: "blue and yellow",
+        choiceD: "pink and yellow",
+        correct: "C"
+    },
+    {
+        question: "How long does a 'polar' day last from sunrise to sunset?",
+        choiceA: "several weeks",
+        choiceB: "six months",
+        choiceC: "one day",
+        choiceD: "one year",
+        correct: "B"
+    },
+    {
+        question: "What time zone is the south pole in?",
+        choiceA: "greenwich mean time (uk)",
+        choiceB: "none, because many time zones meet there",
+        choiceC: "eastern time zone (new york)",
+        choiceD: "pacific standard time",
+        correct: "B"
+    },
+    {
+        question: "what is a flock of crows called?",
+        choiceA: "murder",
+        choiceB: "flock",
+        choiceC: "skool",
+        choiceD: "pack",
+        correct: "A"
+    },
+    {
+        question: "what lives in a sett?",
+        choiceA: "rabbit",
+        choiceB: "badger",
+        choiceC: "butterfly",
+        choiceD: "fox",
+        correct: "B"
+    }
+
+];
