33acd0513a8f76921698751d85c06c25694ccdf3
JHodgkins <jhodgkins78@gmail.com>
2022-05-15 01:29:07 +0000

    code: added function to change to game section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-// function URL() {
-//     location.href = 'https://youtube.com';
-// }
-document.getElementById("play_now").onclick = function () {
-    location.href = "game.html";
-};
+// select game elements
+const startGame = document.getElementById("start_game");
+const intro = document.getElementById("intro");
+const gameLoad = document.getElementById("gameLoad");
+
+// Game start
+function gameStart() {
+    intro.style.display = 'none';
+    gameLoad.style.display = 'block';
+}
+
+// event listeners
+startGame.addEventListener('click', gameStart);
