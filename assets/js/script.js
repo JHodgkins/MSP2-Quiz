08adf852111633a57407cf39048701fc62f57af0
JHodgkins <jhodgkins78@gmail.com>
2022-05-15 17:12:50 +0000

    code: added state change for nav play link

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@
 const startGame = document.getElementById("start_game");
 const intro = document.getElementById("intro");
 const gameLoad = document.getElementById("gameLoad");
+const playBtn = document.getElementById("play");
 
 // Game start
 function gameStart() {
     intro.style.display = 'none';
     gameLoad.style.display = 'block';
+    playBtn.innerText = 'Restart QuizBox';
 }
 
 // event listeners
