dd27af6643b5523275eab58fd260402dcd5745eb
JHodgkins <jhodgkins78@gmail.com>
2022-05-19 15:24:42 +0000

    code: additional questions added

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,42 +1,162 @@
 let questions = [
     {
-        question: "what two colours make green??",
-        choiceA: "red and blue",
-        choiceB: "blue and purple",
-        choiceC: "blue and yellow",
-        choiceD: "pink and yellow",
+        question: "What two colours make green??",
+        choiceA: "Red and blue",
+        choiceB: "Blue and purple",
+        choiceC: "Blue and yellow",
+        choiceD: "Pink and yellow",
         correct: "C"
     },
     {
         question: "How long does a 'polar' day last from sunrise to sunset?",
-        choiceA: "several weeks",
-        choiceB: "six months",
-        choiceC: "one day",
-        choiceD: "one year",
+        choiceA: "Several weeks",
+        choiceB: "Six months",
+        choiceC: "One day",
+        choiceD: "One year",
         correct: "B"
     },
     {
         question: "What time zone is the south pole in?",
-        choiceA: "greenwich mean time (uk)",
-        choiceB: "none, because many time zones meet there",
-        choiceC: "eastern time zone (new york)",
-        choiceD: "pacific standard time",
+        choiceA: "Greenwich mean time (uk)",
+        choiceB: "None, because many time zones meet there",
+        choiceC: "Eastern time zone (new york)",
+        choiceD: "Pacific standard time",
         correct: "B"
     },
     {
-        question: "what is a group of crows called?",
-        choiceA: "murder",
-        choiceB: "flock",
-        choiceC: "skool",
-        choiceD: "pack",
+        question: "What is a group of crows called?",
+        choiceA: "Murder",
+        choiceB: "Flock",
+        choiceC: "Skool",
+        choiceD: "Pack",
         correct: "A"
     },
     {
-        question: "what lives in a sett?",
-        choiceA: "rabbit",
-        choiceB: "badger",
-        choiceC: "butterfly",
-        choiceD: "fox",
+        question: "What lives in a sett?",
+        choiceA: "Rabbit",
+        choiceB: "Badger",
+        choiceC: "Butterfly",
+        choiceD: "Fox",
+        correct: "B"
+    },
+    {
+        question: "In which country is the Taj Mahal?",
+        choiceA: "China",
+        choiceB: "France",
+        choiceC: "Australia",
+        choiceD: "India",
+        correct: "D"
+    },
+    {
+        question: "How many sides does a hexagon have?",
+        choiceA: "5",
+        choiceB: "6",
+        choiceC: "7",
+        choiceD: "8",
+        correct: "B"
+    },
+    {
+        question: "What sort of animal is the video game character Sonic?",
+        choiceA: "Hedgehog",
+        choiceB: "Fox",
+        choiceC: "Tiger",
+        choiceD: "Monkey",
+        correct: "A"
+    },
+    {
+        question: "Which fairy tale character slept for 100 years?",
+        choiceA: "Cinderella",
+        choiceB: "Snow White",
+        choiceC: "Sleeping Beauty ",
+        choiceD: "Princess Tiana",
+        correct: "C"
+    },
+    {
+        question: "What is the name of the organ that pumps blood around the body?",
+        choiceA: "Lungs",
+        choiceB: "Heart",
+        choiceC: "Liver",
+        choiceD: "Kidneys",
+        correct: "B"
+    },
+    {
+        question: "In The Jungle Book what kind of animal is Baloo?",
+        choiceA: "Jaguar",
+        choiceB: "Snake",
+        choiceC: "Bear",
+        choiceD: "Orangutang",
+        correct: "C"
+    },
+    {
+        question: "What is the coloured part of the eye that surrounds the pupil?",
+        choiceA: "Lens",
+        choiceB: "Iris",
+        choiceC: "Cornea",
+        choiceD: "Retina",
+        correct: "B"
+    },
+    {
+        question: "Which famous building did Guy Fawkes try to blow up?",
+        choiceA: "Big Ben",
+        choiceB: "Buckingham palace",
+        choiceC: "The tower of London",
+        choiceD: "The houses of parliament",
+        correct: "D"
+    },
+    {
+        question: "What is the largest planet in the solar system?",
+        choiceA: "Earth",
+        choiceB: "Saturn",
+        choiceC: "Jupiter",
+        choiceD: "Neptune",
+        correct: "C"
+    },
+    {
+        question: "What is the third planet from the sun?",
+        choiceA: "Mercury",
+        choiceB: "Venus",
+        choiceC: "Mars",
+        choiceD: "Earth",
+        correct: "D"
+    },
+    {
+        question: "Which chess piece can only move diagonally?",
+        choiceA: "King",
+        choiceB: "Pawn",
+        choiceC: "Bishop",
+        choiceD: "Queen",
+        correct: "C"
+    },
+    {
+        question: "What colour is a giraffe's tongue?",
+        choiceA: "Pink",
+        choiceB: "Yellow",
+        choiceC: "Blue",
+        choiceD: "Black",
+        correct: "C"
+    },
+    {
+        question: "How many planets are there in the Solar System?",
+        choiceA: "7",
+        choiceB: "8",
+        choiceC: "9",
+        choiceD: "10",
+        correct: "B"
+    },
+    {
+        question: "What is the worlds largest country by area?",
+        choiceA: "Chile",
+        choiceB: "Japan",
+        choiceC: "Australia",
+        choiceD: "Russia",
+        correct: "D"
+    },
+    {
+        question: "What is the worlds biggest hot desert?",
+        choiceA: "Gobi",
+        choiceB: "Sahara",
+        choiceC: "Arabian",
+        choiceD: "Kalahari",
         correct: "B"
     }
 
